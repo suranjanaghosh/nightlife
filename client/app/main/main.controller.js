02b2584dff55e38aab96b0fb3b8fb8fed6b09992
Vince Cooley <vince.r.cooley@gmail.com>
2016-01-21 22:18:16 -0600

    Refactored search directive and MainController

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,30 +3,6 @@
 
 angular.module('nightlifeApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.error = 'Hello';
-    $scope.messages = [];
-    $scope.searchTerm = 'Where are you?';
-    $scope.searchResults = {};
 
-    $scope.submitSearch = function() {
-      if($scope.searchTerm === '') { $scope.searchTerm = 'Waco, TX'; }
-      var encoded = encodeURIComponent($scope.searchTerm);
-      $http.get('/search?location=' + encoded)
-        .then(function successCallback(res) {
-          $scope.searchTerm = '';
-          $scope.searchResults = res.data;
-        }, function errorCallback(res) {
-          if (res.status === 404) {
-            $scope.error = 'That location was not found.';
-          }
-          else {
-            $scope.error = 'There was some error searching for that location.';
-          }
-        });
-    };
-
-    $scope.clearError = function() {
-      $scope.error = '';
-    };
 
   });
--- client/app/search/search.directive.js
+++ client/app/search/search.directive.js
@@ -1,10 +1,39 @@
 'use strict';
 
 angular.module('nightlifeApp')
+  .controller('SearchController', function ($scope, $http, socket) {
+    $scope.error = 'Hello';
+    $scope.messages = [];
+    $scope.searchTerm = 'Where are you?';
+    $scope.searchResults = {};
+
+    $scope.submitSearch = function() {
+      if($scope.searchTerm === '') { $scope.searchTerm = 'Waco, TX'; }
+      var encoded = encodeURIComponent($scope.searchTerm);
+      $http.get('/search?location=' + encoded)
+        .then(function successCallback(res) {
+          $scope.searchTerm = '';
+          $scope.searchResults = res.data;
+        }, function errorCallback(res) {
+          if (res.status === 404) {
+            $scope.error = 'That location was not found.';
+          }
+          else {
+            $scope.error = 'There was some error searching for that location.';
+          }
+        });
+    };
+
+    $scope.clearError = function() {
+      $scope.error = '';
+    };
+  })
+
   .directive('search', function () {
     return {
       templateUrl: 'app/search/search.html',
       restrict: 'EA',
+      controller: 'SearchController',
       link: function (scope, element, attrs) {
       }
     };
